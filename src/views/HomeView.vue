7573a44c909af501011458fe1270900ae3796255
strbagus <satrio.baguswbw@gmail.com>
2022-10-03 10:00:17 +0700

    baseline commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+<script lang="js">
+  
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-yellow-500 text-3xl">Spotify</h1>
   </main>
 </template>
