1c605d4935e8901eb8b606016bdc4cfc0f3e32ab
strbagus <satrio.baguswbw@gmail.com>
2022-10-09 22:12:02 +0700

    Call My Playlist

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
   import { RouterLink } from 'vue-router';
   export default {
     mounted() {
-      if(localStorage.getItem('authcode')){
+      if(!localStorage.getItem('authCode')||localStorage.getItem('authCode').accessToken===null){
         this.logout()
       }else{
         this.localToData()
-        this.spotUser()
+        this.spotCallAPI()
         this.toogle = true
       }
     },
@@ -23,6 +23,7 @@
         },
         toogle: false,
         user: [],
+        playlists: [],
       }
     },
     methods: {
@@ -33,7 +34,7 @@
         this.authCode.expiredIn = authCode.expiredIn
         this.authCode.state = authCode.state
       },
-      spotUser: function(){
+      spotCallAPI: function(){
         fetch('https://api.spotify.com/v1/me', {
           method: 'get',
           headers: {
@@ -44,6 +45,19 @@
           .then((response) => response.json())
           .then((data) => {
             this.user = data
+            console.log(data)
+          })
+
+        fetch('https://api.spotify.com/v1/me/playlists?limit=4', {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+this.authCode.accessToken,
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            this.playlists = data
             console.log(data)
           })
       },
@@ -59,6 +73,15 @@
   <main>
     <h1 class="text-green-500 text-3xl italic text-center py-2 font-bold">Statify</h1>
     <div v-if="toogle===true" class="content-box container mx-auto absolute p-2">
+      <div class="flex bg-yellow-500 py-3 px-1">
+        <div v-for="playlist in playlists.items" class="bg-red-500 flex flex-wrap">
+          <div class="mx-3">
+            <img :src="playlist.images[2].url" alt="">
+            <h3>{{ playlist.name }}</h3>
+            <span class="text-xs">{{ playlist.owner.display_name+' '+playlist.tracks.total}}</span>
+          </div>
+        </div>
+      </div>
       <div class="flex bg-white rounded-xl p-2 items-center">
         <img :src="user.images[0].url" alt="test" class="h-10 w-10 rounded-xl">
         <span class="text-xl px-3 text-gray-600 font-semibold italic">{{ user.display_name }}</span>
