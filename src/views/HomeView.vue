efaa404611dfcae3b716b031e9dba13f6f9ba05f
strbagus <satrio.baguswbw@gmail.com>
2022-10-10 11:09:31 +0700

    UI for Top Artists

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -76,8 +76,23 @@
 <template>
   <main>
     <h1 class="text-green-500 text-3xl italic text-center py-2 font-bold">Statify</h1>
-    <div v-if="toogle===true" class="content-box container mx-auto absolute p-2">
-      <div class="flex bg-yellow-500 py-3 px-1">
+    <div v-if="toogle===true" class="content-box container mx-auto absolute p-2 bg-blue-500 w-full md:w-1/2 lg:w-1/3">
+      <div class="flex flex-col bg-green-500 py-3 px-1">
+        <div class="flex bg-red-500 w-full justify-evenly">
+          <div class="flex items-end bg-green-500 w-24 justify-center"><img :src="user.images[0].url" alt="" class="h-20 w-20 rounded-full"></div>
+          <div class="bg-blue-500 w-24 justify-center"><img :src="user.images[0].url" alt="" class="h-24 w-24 rounded-full"></div>
+          <div class="flex items-end bg-yellow-500 w-24 justify-center"><img :src="user.images[0].url" alt="" class="h-16 w-16 rounded-full"></div>
+        </div>
+        <div class="flex bg-red-500 w-full justify-evenly">
+          <div class="flex items-start bg-green-500 w-24 justify-center"><span class="text-lg">Bol4</span></div>
+          <div class="flex items-start bg-green-500 w-24 justify-center"><span class="text-xl">IU</span></div>
+          <div class="flex items-start bg-green-500 w-24 justify-center"><span class="text-md">KimSejeong</span></div>
+        </div>
+      </div>
+      <div class="flex flex-col bg-red-500 py-3 px-1 w-full">
+        genre
+      </div>
+      <div class="flex bg-yellow-500 py-3 px-1 justify-evenly">
         <div v-for="playlist in playlists.items" class="bg-red-500 flex flex-wrap">
           <div class="mx-3">
             <img :src="playlist.images[2].url" alt="">
