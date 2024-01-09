7c0dc875f1da66bf4ab72b784f1738a845cfd961
Satrio Bagus <strbagus@deblp.local>
2024-01-09 04:45:26 -0800

    change import in router to code spliting

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import LoginView from "../views/LoginView.vue";
-import CallbackView from "../views/CallbackView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,17 +6,17 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("@/views/HomeView.vue"),
     },
     {
       path: "/login",
       name: "login",
-      component: LoginView,
+      component: () => import("@/views/LoginView.vue"),
     },
     {
       path: "/callback",
       name: "callback",
-      component: CallbackView,
+      component: () => import("@/views/CallbackView.vue"),
     },
   ],
 });
